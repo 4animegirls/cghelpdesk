4dfd13e28b1ca25af9ca3c965a1d8bb83bec35e5
Marko Tomčík <markotomcik@gmail.com>
2021-07-27 13:57:31 +0200

    button

--- components/home/Home.js
+++ components/home/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Layout, Text } from '@ui-kitten/components';
+import { ButtonSimpleUsageShowcase } from './Button';
 
 export default class Home extends Component {
   render() {
     return (
       <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text category='h1'>FACE IN THE CULTURE OF FACELESSNESS</Text>
+        <ButtonSimpleUsageShowcase />
       </Layout>
     );
   }
