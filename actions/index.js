85ab43916cab99f2493e97ad95abf155d3cc9dae
Samuel Manik <79486973+Pluvas159@users.noreply.github.com>
2021-08-03 10:19:36 +0200

    added utils and login fetch

--- actions/index.js
+++ actions/index.js
@@ -1,3 +1,5 @@
+import { Login } from '../utils'
+
 export const addToken = (token) => ({
     type : 'ADD_TOKEN',
     payload: { token }
@@ -6,3 +8,29 @@ export const addToken = (token) => ({
 export const removeToken = () => ({
     type: 'REMOVE_TOKEN'
 });
+
+
+export const login = (userLogin) => {
+    return async (dispatch) => {
+        try {
+            dispatch(loginRequest())
+            let response = await login(userLogin);
+            dispatch(loginSuccess(response))
+        } catch {
+            loginFailure()
+        }
+    }
+}
+
+const loginRequest = () => ({
+    type: 'LOGIN_REQUEST'
+});
+
+const loginSuccess = (response) => ({
+    type: 'LOGIN_SUCCESS',
+    payload: { response }
+})
+
+const loginFailure = () => {
+    type : 'LOGIN_FAILURE'
+}
