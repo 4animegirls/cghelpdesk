6d3f4d48e3c4483a90e3ea87268f98659dcbd3b1
Samuel Manik <79486973+Pluvas159@users.noreply.github.com>
2021-08-03 14:25:21 +0200

    added user reducer

--- reducers/statusReducer.js
+++ reducers/statusReducer.js
@@ -1,24 +1,9 @@
-import actionTypes from '../actions/actionTypes';
+import actionTypes from '...actions/actionTypes';
 
-const status = (state = {theme: 'dark', user: {Token: null, username: ''}, page: 'login'}, action) => {
+const status = (state = {page: 'login'}, action) => {
     switch(action.type){
-        case 'ADD_USERNAME':
-            return {...state, user:{username: action.payload.username} };
-
-        case 'ADD_TOKEN':
-            return {...state, user:{Token: action.payload.token}};
-
-        case 'REMOVE_TOKEN':
-            return {...state, user:{Token: null}};
-        
-        case 'CHANGE_THEME':
-            return {...state, theme: action.payload.theme};
-
         case 'CHANGE_PAGE':
             return {...state, page: action.payload.page};
-
-        case actionTypes.LOGIN_SUCCESS:
-            return {...state, user:{Token: action.payload.response.Data.Token, refreshToken: action.payload.response.Data.RefreshToken}};
 
         default:
             return state;
