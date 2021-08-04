a4cb514fc3b8a76a60d1eeb008dbc1e9b009fce1
Samuel Manik <79486973+Pluvas159@users.noreply.github.com>
2021-08-04 15:02:28 +0200

    Update loadingReducer.js

--- reducers/loadingReducer.js
+++ reducers/loadingReducer.js
@@ -1,14 +1,16 @@
 import actionTypes from '../actions/actionTypes';
 
-export default loading = (status = false, action) => {
+const loading = (status = false, action) => {
     switch (action.type) {
         case actionTypes.ADD_ITEMS_LOADING:
-            return true
+            return true;
 
         case actionTypes.ADD_ITEMS_SUCCESS:
-            return false
+            return false;
 
         case actionTypes.ADD_ITEMS_FAILURE:
-            return false
+            return false;
     }
 }
+
+export default loading;
