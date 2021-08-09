59c5c9ebb8a02d643f42aff214b14aad28bccc85
Pluvas159 <maniksamuel159@gmail.com>
2021-08-09 09:10:17 +0200

    separate DrawerNavigation

---
+++ components/navigation/DrawerNavigator.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import Home from '../home/Home'
+import { removeToken } from '../../actions'
+import Settings from '../settings/Settings'
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { IndexPath, Layout, Drawer, DrawerItem, Text, Icon } from '@ui-kitten/components';
+import { useDispatch } from 'react-redux'
+
+
+const { Navigator, Screen } = createDrawerNavigator();
+
+const Header = () => (
+    <Layout style={{ paddingTop: 60, paddingBottom: 20, paddingLeft: 20 }}>
+        <Text category='h1' style={{ fontSize: 30, fontFamily: 'serif', margin: 0, padding: 0 }}>coradesk</Text>
+    </Layout>
+);
+
+
+const DrawerContent = ({ navigation, state }) => {
+    const dispatch = useDispatch();
+    return (
+        <Drawer
+            header={Header}
+            selectedIndex={new IndexPath(state.index)}
+            onSelect={index => navigation.navigate(state.routeNames[index.row])}>
+            <DrawerItem title='Home' />
+            <DrawerItem title='Settings' />
+            <DrawerItem title='Logout' onPress={
+                () => { dispatch(removeToken()) }} style={{ backgroundColor: 'darkred' }} accessoryRight={<Icon name='close-square' />} />
+        </Drawer>
+    )
+};
+
+export default class DrawerNavigator extends React.Component {
+    constructor({ navigation }) {
+        super()
+    }
+
+    render() {
+        return (
+            <Navigator
+                screenOptions={({ navigation }) => ({
+                    headerShown: false,
+                    gestureEnabled: true
+                })} drawerContent={props => <DrawerContent {...props} />}
+            >
+                <Screen name='Home' component={Home} />
+                <Screen name='Settings' component={Settings} />
+                <Screen name='Logout' component={Screen} />
+            </Navigator>
+        );
+    }
+}
--- components/navigation/Navigation.js
+++ components/navigation/Navigation.js
@@ -1,60 +1,12 @@
 import React, { Component } from 'react';
 import { NavigationContainer, StackActions } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack'
-import { IndexPath, Layout, Drawer, DrawerItem, Text, Icon } from '@ui-kitten/components';
-import { useDispatch, connect } from 'react-redux'
-import Home from '../home/Home'
-import { removeToken } from '../../actions'
-import Settings from '../settings/Settings'
+import {connect } from 'react-redux'
 import Details from '../details/Details';
 import Login from '../login/Login'
+import DrawerNavigator from './DrawerNavigator'
 
-
-const { Navigator, Screen } = createDrawerNavigator();
 const Stack = createStackNavigator()
-
-const Header = () => (
-  <Layout style={{ paddingTop: 60, paddingBottom: 20, paddingLeft: 20 }}>
-    <Text category='h1' style={{ fontSize: 30, fontFamily: 'serif', margin: 0, padding: 0 }}>coradesk</Text>
-  </Layout>
-);
-
-const DrawerContent = ({ navigation, state }) => {
-  const dispatch = useDispatch();
-  return (
-    <Drawer
-      header={Header}
-      selectedIndex={new IndexPath(state.index)}
-      onSelect={index => navigation.navigate(state.routeNames[index.row])}>
-      <DrawerItem title='Home' />
-      <DrawerItem title='Settings' />
-      <DrawerItem title='Logout' onPress={
-        () => { dispatch(removeToken()) }} style={{ backgroundColor: 'darkred' }} accessoryRight={<Icon name='close-square' />} />
-    </Drawer>
-  )
-};
-
-export class DrawerNavigator extends Component {
-  constructor({ navigation }) {
-    super()
-  }
-
-  render() {
-    return (
-      <Navigator
-        screenOptions={({ navigation }) => ({
-          headerShown: false,
-          gestureEnabled: true
-        })} drawerContent={props => <DrawerContent {...props} />}
-      >
-        <Screen name='Home' component={Home} />
-        <Screen name='Settings' component={Settings} />
-        <Screen name='Logout' component={Screen} />
-      </Navigator>
-    );
-  }
-}
 
 class AppNavigator extends Component {
   render() {
