f3d43a4787f3b40618bf0d8dc6350add38baeaae
Svaagi <jozefsvagerkom5@gmail.com>
2021-08-10 15:18:10 +0200

    small changes loader

--- components/login/LoginLoader.js
+++ components/login/LoginLoader.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Text } from '@ui-kitten/components';
-import { View, TouchableOpacity, ActivityIndicator } from 'react-native'
+import { TouchableOpacity, ActivityIndicator, View } from 'react-native'
+import { Text, Button } from '@ui-kitten/components';
 import { loginAction } from '../../actions';
 
 
@@ -14,20 +14,24 @@ class LoginLoader extends Component {
     }
   }
 
+  loginAction = ({ Username, Password }) => {
+    return { Token: 'fdfdgdfrg4', username: 'svagi' };
+  }
 
   doSignup = async () => {
-    const Username =  this.props.Username
+    const Username = this.props.Username
     const Password = this.props.Password
     this.setState({ isLoading: true });
-    await this.props.loginAction({ Username, Password });
+    await this.loginAction({ Username, Password });
     this.setState({ isLoading: false })
+
   };
 
   render() {
     return (
       <View>
         <TouchableOpacity onPress={this.doSignup}>
-          <Text>LOG IN</Text>
+          <Text>LOG IN </Text>
         </TouchableOpacity >
         <ActivityIndicator animating={this.state.isLoading} />
       </View>
