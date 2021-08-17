833b98130a01c2e262fde3e3e76834a8ec0d018b
LonziSlovakia <lonzikormos@gmail.com>
2021-08-17 09:20:33 +0200

    BAckup befrore merge

--- components/navigation/Navigation.js
+++ components/navigation/Navigation.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack'
-import {connect } from 'react-redux'
+import { connect } from 'react-redux'
 import Details from '../details/Details';
 import Login from '../login/Login'
 import DrawerNavigator from './DrawerNavigator'
+import { View } from 'react-native';
 
 const Stack = createStackNavigator()
 
@@ -12,16 +13,18 @@ class AppNavigator extends Component {
   render() {
 
     return (
-      <NavigationContainer >
-        <Stack.Navigator>
-          {(this.props.user.Token === null)
-            && <Stack.Screen name='Login' component={Login} options={{ headerShown: false }} />}
-          {(this.props.user.Token !== null)
-            && <Stack.Screen name='root' component={DrawerNavigator} options={{ headerShown: false }} />}
-          {(this.props.user.Token !== null)
-            && <Stack.Screen name='Details' component={Details} options={{ headerShown: false }}/>}
-        </Stack.Navigator>
-      </NavigationContainer>
+      <View style={{ background: '#222B45' }}>
+        <NavigationContainer >
+          <Stack.Navigator>
+            {(this.props.user.Token === null)
+              && <Stack.Screen name='Login' component={Login} options={{ headerShown: false }} />}
+            {(this.props.user.Token !== null)
+              && <Stack.Screen name='root' component={DrawerNavigator} options={{ headerShown: false }} />}
+            {(this.props.user.Token !== null)
+              && <Stack.Screen name='Details' component={Details} options={{ headerShown: false }} />}
+          </Stack.Navigator>
+        </NavigationContainer>
+      </View>
     )
   }
 };
